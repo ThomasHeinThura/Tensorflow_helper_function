3af795526bcd4a4a71b506b8f6e3ca0be9010170
ThomasHeinThura <alexzander125.alex@gmail.com>
2022-12-27 23:01:46 +0630

    timeseries

--- for_time_series.py
+++ for_time_series.py
@@ -1,1 +1,22 @@
 #for_time_series
+
+"""import from pandas
+# Import with pandas 
+import pandas as pd
+# Parse dates and set date column to index
+df = pd.read_csv("/content/BTC_USD_2013-10-01_2021-05-18-CoinDesk.csv", 
+                 parse_dates=["Date"], 
+                 index_col=["Date"]) # parse the date column (tell pandas column 1 is a datetime)
+df.head()
+
+# Only want closing price for each day 
+bitcoin_prices = pd.DataFrame(df["Closing Price (USD)"]).rename(columns={"Closing Price (USD)": "Price"})
+bitcoin_prices.head()
+
+import matplotlib.pyplot as plt
+bitcoin_prices.plot(figsize=(10, 7))
+plt.ylabel("BTC Price")
+plt.title("Price of Bitcoin from 1 Oct 2013 to 18 May 2021", fontsize=16)
+plt.legend(fontsize=14);
+"""
+

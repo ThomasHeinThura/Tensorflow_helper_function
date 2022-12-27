928e9eb96b5e0b781539875f2b6204b82d414d69
ThomasHeinThura <alexzander125.alex@gmail.com>
2022-12-27 23:58:40 +0630

    ensemble and others

--- Advance_models_for_time_series.py
+++ Advance_models_for_time_series.py
@@ -229,4 +229,129 @@ def get_ensemble_models(horizon=HORIZON,
 
   return ensemble_models # return list of trained models
 
+"""% ensemble model train
+# Get list of trained ensemble models
+ensemble_models = get_ensemble_models(num_iter=5,
+                                      num_epochs=1000)
+"""
+# Create a function which uses a list of trained models to make and return a list of predictions
+def make_ensemble_preds(ensemble_models, data):
+  ensemble_preds = []
+  for model in ensemble_models:
+    preds = model.predict(data) # make predictions with current ensemble model
+    ensemble_preds.append(preds)
+  return tf.constant(tf.squeeze(ensemble_preds))
 
+"""# Create a list of ensemble predictions
+ensemble_preds = make_ensemble_preds(ensemble_models=ensemble_models,
+                                     data=test_dataset)
+ensemble_preds
+
+# Evaluate ensemble model(s) predictions
+ensemble_results = evaluate_preds(y_true=y_test,
+                                  y_pred=np.median(ensemble_preds, axis=0)) # take the median across all ensemble predictions
+ensemble_results
+"""
+# Find upper and lower bounds of ensemble predictions
+def get_upper_lower(preds): # 1. Take the predictions of multiple randomly initialized deep learning neural networks
+  
+  # 2. Measure the standard deviation of the predictions
+  std = tf.math.reduce_std(preds, axis=0)
+  
+  # 3. Multiply the standard deviation by 1.96
+  interval = 1.96 * std # https://en.wikipedia.org/wiki/1.96 
+
+  # 4. Get the prediction interval upper and lower bounds
+  preds_mean = tf.reduce_mean(preds, axis=0)
+  lower, upper = preds_mean - interval, preds_mean + interval
+  return lower, upper
+
+# Get the upper and lower bounds of the 95% 
+lower, upper = get_upper_lower(preds=ensemble_preds)
+
+# Get the median values of our ensemble preds
+ensemble_median = np.median(ensemble_preds, axis=0)
+
+# Plot the median of our ensemble preds along with the prediction intervals (where the predictions fall between)
+offset=500
+plt.figure(figsize=(10, 7))
+plt.plot(X_test.index[offset:], y_test[offset:], "g", label="Test Data")
+plt.plot(X_test.index[offset:], ensemble_median[offset:], "k-", label="Ensemble Median")
+plt.xlabel("Date")
+plt.ylabel("BTC Price")
+plt.fill_between(X_test.index[offset:], 
+                 (lower)[offset:], 
+                 (upper)[offset:], label="Prediction Intervals")
+plt.legend(loc="upper left", fontsize=14);
+
+
+# 1. Create function to make predictions into the future
+def make_future_forecast(values, model, into_future, window_size=WINDOW_SIZE) -> list:
+  """
+  Makes future forecasts into_future steps after values ends.
+
+  Returns future forecasts as list of floats.
+  """
+  # 2. Make an empty list for future forecasts/prepare data to forecast on
+  future_forecast = []
+  last_window = values[-WINDOW_SIZE:] # only want preds from the last window (this will get updated)
+
+  # 3. Make INTO_FUTURE number of predictions, altering the data which gets predicted on each time 
+  for _ in range(into_future):
+    
+    # Predict on last window then append it again, again, again (model starts to make forecasts on its own forecasts)
+    future_pred = model.predict(tf.expand_dims(last_window, axis=0))
+    print(f"Predicting on: \n {last_window} -> Prediction: {tf.squeeze(future_pred).numpy()}\n")
+    
+    # Append predictions to future_forecast
+    future_forecast.append(tf.squeeze(future_pred).numpy())
+    # print(future_forecast)
+
+    # Update last window with new pred and get WINDOW_SIZE most recent preds (model was trained on WINDOW_SIZE windows)
+    last_window = np.append(last_window, future_pred)[-WINDOW_SIZE:]
+  
+  return future_forecast
+
+"""# Make forecasts into future of the price of Bitcoin
+# Note: if you're reading this at a later date, you may already be in the future, so the forecasts 
+# we're making may not actually be forecasts, if that's the case, readjust the training data.
+future_forecast = make_future_forecast(values=y_all,
+                                       model=model_9,
+                                       into_future=INTO_FUTURE,
+                                       window_size=WINDOW_SIZE)
+
+future_forecast[:10]
+  
+"""
+
+def get_future_dates(start_date, into_future, offset=1):
+  """
+  Returns array of datetime values from ranging from start_date to start_date+horizon.
+
+  start_date: date to start range (np.datetime64)
+  into_future: number of days to add onto start date for range (int)
+  offset: number of days to offset start_date by (default 1)
+  """
+  start_date = start_date + np.timedelta64(offset, "D") # specify start date, "D" stands for day
+  end_date = start_date + np.timedelta64(into_future, "D") # specify end date
+  return np.arange(start_date, end_date, dtype="datetime64[D]") # return a date range between start date and end date
+
+"""# Last timestep of timesteps (currently in np.datetime64 format)
+last_timestep = bitcoin_prices.index[-1]
+last_timestep
+
+# Get next two weeks of timesteps
+next_time_steps = get_future_dates(start_date=last_timestep, 
+                                   into_future=INTO_FUTURE)
+next_time_steps
+
+# Insert last timestep/final price so the graph doesn't look messed
+next_time_steps = np.insert(next_time_steps, 0, last_timestep)
+future_forecast = np.insert(future_forecast, 0, btc_price[-1])
+next_time_steps, future_forecast
+
+# Plot future price predictions of Bitcoin
+plt.figure(figsize=(10, 7))
+plot_time_series(bitcoin_prices.index, btc_price, start=2500, format="-", label="Actual BTC Price")
+plot_time_series(next_time_steps, future_forecast, format="-", label="Predicted BTC Price")
+"""

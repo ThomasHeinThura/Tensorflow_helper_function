de7022423a064013613abf941b0aaa063d64696d
ThomasHeinThura <alexzander125.alex@gmail.com>
2022-12-28 20:27:46 +0630

    fix some code clone all-model to function add import in cnn nlp and time series

--- all_model_functions.py
+++ all_model_functions.py
@@ -1,36 +1,3 @@
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -# 
-#   # This is for helping the exam and others things make more easy #   #
-# 1. for import data #
-# 1.1 import from local data
-# 1.2 improt from cvs
-# 1.3 import from online data
-# 1.4 import from tf.keras.dataset
-#
-# 2. for visualize import data #
-# 2.1 visualize plot data
-# 2.2 visualize picture
-# 2.3 visualize words and panda
-#
-# 3. for preparation data (normalize and add to pipeline)
-# 3.1 data work thorugh for CNN
-# 3.2 datagen for CNN
-# 3.3 token and embedd for NLP 
-# 3.4 windows and horizon for timeseries
-# 3.5 data for classification and regression 
-#
-# 4. Fit the model and make sure to remember history and callbacks 
-# 4.1 early stopping callbacks
-# 4.2 plateua for learning rate reducing
-# 4.3 save the best perfromance models 
-#
-# 5. visualize the model and plot prediction and matrix
-# 5.1 model visualization
-# 5.2 plot prediction 
-# 5.3 confusion matrix 
-# 5.4 the most common mistakes (ture labels and prediction show) for CNN and NLP
-# 5.5 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -# 
-# import for python 
 import tensorflow as tf
 from datetime import datetime
 import matplotlib.pyplot as plt
@@ -46,7 +13,35 @@ import random
 tf.get_logger().setLevel('ERROR')
 """
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -# 
+# 1. for import data #
+# 1.1 import from local data
+def unzip_data(filename):
+  """
+  Unzips filename into the current working directory.
 
+  Args:
+    filename (str): a filepath to a target zip folder to be unzipped.
+  """
+  zip_ref = zipfile.ZipFile(filename, "r")
+  zip_ref.extractall()
+  zip_ref.close()
+# data work thorugh dirpath for CNN
+def walk_through_dir(dir_path):
+  """
+  Walks through dir_path returning its contents.
+
+  Args:
+    dir_path (str): target directory
+  
+  Returns:
+    A print out of:
+      number of subdiretories in dir_path
+      number of images (files) in each subdirectory
+      name of each subdirectory
+  """
+  for dirpath, dirnames, filenames in os.walk(dir_path):
+    print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+  
 # 1.2 improt from cvs
 """ import from cvs:
 import pandas as pd
@@ -101,6 +96,25 @@ def show_methods_for_import_online():
   X_train_normal = ct.transform(X_train)
   X_test_normal = ct.transform(X_test)
 
+  """
+
+# 1.4 import from tf.keras.dataset
+def show_methods_for_import_dataset():
+  """
+  import tensorflow as tf
+  from tensorflow.keras.datasets import fashion_mnist
+
+  # The data has already been sorted into training and test sets for us
+  (train_data, train_labels), (test_data, test_labels) = fashion_mnist.load_data()
+
+  # Viewing the single example for fashion_mnist
+  # Plot a single example
+  import matplotlib.pyplot as plt
+  plt.imshow(train_data[7]);
+
+  # Plot an example image and its label
+  plt.imshow(train_data[17], cmap=plt.cm.binary) # change the colours to black & white
+  plt.title(class_names[train_labels[17]]);
   """
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -# 
@@ -158,7 +172,7 @@ def view_random_image(target_dir, target_class):
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -#
 # 2.4 visualize words and pandas
-""" 
+"""
 # Download data (same as from Kaggle)
 !wget "https://storage.googleapis.com/ztm_tf_course/nlp_getting_started.zip"
 
--- for_nlp.py
+++ for_nlp.py
@@ -34,31 +34,45 @@
 # 1. for import data #
 # 1.1 import from local data
 def unzip_data(filename):
-  """
-  Unzips filename into the current working directory.
+    import zipfile
+    """
+    Unzips filename into the current working directory.
 
-  Args:
-    filename (str): a filepath to a target zip folder to be unzipped.
-  """
-  zip_ref = zipfile.ZipFile(filename, "r")
-  zip_ref.extractall()
-  zip_ref.close()
+    Args:
+        filename (str): a filepath to a target zip folder to be unzipped.
+    """
+    zip_ref = zipfile.ZipFile(filename, "r")
+    zip_ref.extractall()
+    zip_ref.close()
 # data work thorugh dirpath for CNN
 def walk_through_dir(dir_path):
-  """
-  Walks through dir_path returning its contents.
+    import os 
+    """
+    Walks through dir_path returning its contents.
 
-  Args:
-    dir_path (str): target directory
+    Args:
+        dir_path (str): target directory
   
-  Returns:
-    A print out of:
-      number of subdiretories in dir_path
-      number of images (files) in each subdirectory
-      name of each subdirectory
-  """
-  for dirpath, dirnames, filenames in os.walk(dir_path):
-    print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+    Returns:
+        A print out of:
+        number of subdiretories in dir_path
+        number of images (files) in each subdirectory
+        name of each subdirectory
+    """
+    for dirpath, dirnames, filenames in os.walk(dir_path):
+        print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+# Visualize classnes from dir
+def view_class_name_from_dir(path):
+    import pathlib 
+    import numpy as np
+    """
+    View an class name from import data folder
+    :param path:
+    :return print class_name:
+    """
+    data_dir = pathlib.Path(path)
+    class_name = np.array(sorted([item.name for item in data_dir.glob('*')]))
+    return print(class_name)
 
 # 1.2 import from tf.keras.dataset # Procedures
 def show_methods_for_import_dataset():
@@ -79,3 +93,58 @@ def show_methods_for_import_dataset():
   plt.title(class_names[train_labels[17]]);
   """
 
+# 1.3 improt from cvs
+""" import from cvs:
+import pandas as pd
+from tensorflow import tf.datapath = ‘path/to/your/csv/file’
+df = pd.read_csv(path)
+dataset = Dataset.from_tensor_slices(dict(df))
+"""
+
+# 1.4 import from online data
+def show_methods_for_import_online():
+  """
+  This is the example procedure to do in cvs data
+  import pandas as pd
+  # Read in the insurance dataset
+  insurance = pd.read_csv("https://raw.githubusercontent.com/stedy/Machine-Learning-with-R-datasets/master/insurance.csv")
+  # Check out the insurance dataset
+  insurance.head()
+  # Turn all categories into numbers
+  insurance_one_hot = pd.get_dummies(insurance)
+  insurance_one_hot.head() # view the converted columns
+  # Create X & y values
+  X = insurance_one_hot.drop("charges", axis=1)
+  y = insurance_one_hot["charges"]
+  # Create training and test sets
+  from sklearn.model_selection import train_test_split
+  X_train, X_test, y_train, y_test = train_test_split(X, 
+                                                      y, 
+                                                      test_size=0.2, 
+                                                      random_state=42) 
+
+
+  from sklearn.compose import make_column_transformer
+  from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+
+  # Create column transformer (this will help us normalize/preprocess our data)
+  ct = make_column_transformer(
+      (MinMaxScaler(), ["age", "bmi", "children"]), # get all values between 0 and 1
+      (OneHotEncoder(handle_unknown="ignore"), ["sex", "smoker", "region"])
+  )
+
+  # Create X & y
+  X = insurance.drop("charges", axis=1)
+  y = insurance["charges"]
+
+  # Build our train and test sets (use random state to ensure same split as before)
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+  # Fit column transformer on the training data only (doing so on test data would result in data leakage)
+  ct.fit(X_train)
+
+  # Transform training and test data with normalization (MinMaxScalar) and one hot encoding (OneHotEncoder)
+  X_train_normal = ct.transform(X_train)
+  X_test_normal = ct.transform(X_test)
+
+  """

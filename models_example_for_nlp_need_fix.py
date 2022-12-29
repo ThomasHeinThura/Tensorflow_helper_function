3b4fec3e1dcf951ace2c47124a84ae57eb28c2aa
ThomasHeinThura <alexzander125.alex@gmail.com>
2022-12-29 09:08:18 +0630

    split adv model and functions

---
+++ models_example_for_nlp_need_fix.py
@@ -0,0 +1,268 @@
+
+
+# Simple models
+def make_simple_nlp_model():
+    # Set random seed and create embedding layer (new embedding layer for each model)
+    tf.random.set_seed(42)
+    max_vocab_length = 10000 # max number of words to have in our vocabulary
+    max_length = 15 # max length our sequences will be (e.g. how many words from a Tweet does our model see?)
+
+    import tensorflow as tf
+    from tensorflow.keras import layers
+    from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+    model_embedding = layers.Embedding(input_dim=max_vocab_length,
+                                        output_dim=128,
+                                        embeddings_initializer="uniform",
+                                        input_length=max_length,
+                                        name="embedding_2")
+    text_vectorizer = TextVectorization(max_tokens=max_vocab_length,
+                                    output_mode="int",
+                                    output_sequence_length=max_length)
+
+    # Create LSTM model
+    inputs = layers.Input(shape=(1,), dtype="string")
+    x = text_vectorizer(inputs)
+    x = model_embedding(x)
+    print(x.shape)
+    # x = layers.LSTM(64, return_sequences=True)(x) # return vector for each word in the Tweet (you can stack RNN cells as long as return_sequences=True)
+    x = layers.LSTM(64)(x) # return vector for whole sequence
+    print(x.shape)
+    # x = layers.Dense(64, activation="relu")(x) # optional dense layer on top of output of LSTM cell
+    outputs = layers.Dense(1, activation="sigmoid")(x)
+    model = tf.keras.Model(inputs, outputs, name="model_2_LSTM")
+    return model
+
+""" Weight matrix or embedding weight in models
+# Get the vocabulary from the text vectorization layer
+words_in_vocab = text_vectorizer.get_vocabulary()
+len(words_in_vocab), words_in_vocab[:10]
+
+
+# Get the weight matrix of embedding layer 
+# (these are the numerical patterns between the text in the training dataset the model has learned)
+embed_weights = model_1.get_layer("embedding_1").get_weights()[0]
+print(embed_weights.shape) # same size as vocab size and embedding_dim (each word is a embedding_dim size vector)
+"""
+
+""" Naive bayes model to check performance
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+
+# Create tokenization and modelling pipeline
+model_0 = Pipeline([
+                    ("tfidf", TfidfVectorizer()), # convert words to numbers using tfidf
+                    ("clf", MultinomialNB()) # model the text
+])
+
+# Fit the pipeline to the training data
+model_0.fit(train_sentences, train_labels)
+
+baseline_score = model_0.score(val_sentences, val_labels)
+print(f"Our baseline model achieves an accuracy of: {baseline_score*100:.2f}%")
+
+OR
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+
+# Create a pipeline
+model_0 = Pipeline([
+  ("tf-idf", TfidfVectorizer()),
+  ("clf", MultinomialNB())
+])
+
+# Fit the pipeline to the training data
+model_0.fit(X=train_sentences, 
+            y=train_labels_encoded);
+
+"""
+
+""" Basic LSTM model
+# Set random seed and create embedding layer (new embedding layer for each model)
+tf.random.set_seed(42)
+from tensorflow.keras import layers
+model_2_embedding = layers.Embedding(input_dim=max_vocab_length,
+                                     output_dim=128,
+                                     embeddings_initializer="uniform",
+                                     input_length=max_length,
+                                     name="embedding_2")
+
+
+# Create LSTM model
+inputs = layers.Input(shape=(1,), dtype="string")
+x = text_vectorizer(inputs)
+x = model_2_embedding(x)
+print(x.shape)
+# x = layers.LSTM(64, return_sequences=True)(x) # return vector for each word in the Tweet (you can stack RNN cells as long as return_sequences=True)
+x = layers.LSTM(64)(x) # return vector for whole sequence
+print(x.shape)
+# x = layers.Dense(64, activation="relu")(x) # optional dense layer on top of output of LSTM cell
+outputs = layers.Dense(1, activation="sigmoid")(x)
+model_2 = tf.keras.Model(inputs, outputs, name="model_2_LSTM")
+"""
+
+""" GRU model
+# Set random seed and create embedding layer (new embedding layer for each model)
+tf.random.set_seed(42)
+from tensorflow.keras import layers
+model_3_embedding = layers.Embedding(input_dim=max_vocab_length,
+                                     output_dim=128,
+                                     embeddings_initializer="uniform",
+                                     input_length=max_length,
+                                     name="embedding_3")
+
+# Build an RNN using the GRU cell
+inputs = layers.Input(shape=(1,), dtype="string")
+x = text_vectorizer(inputs)
+x = model_3_embedding(x)
+# x = layers.GRU(64, return_sequences=True) # stacking recurrent cells requires return_sequences=True
+x = layers.GRU(64)(x) 
+# x = layers.Dense(64, activation="relu")(x) # optional dense layer after GRU cell
+outputs = layers.Dense(1, activation="sigmoid")(x)
+model_3 = tf.keras.Model(inputs, outputs, name="model_3_GRU")
+"""
+
+""" Bilateral LSTM
+# Set random seed and create embedding layer (new embedding layer for each model)
+tf.random.set_seed(42)
+from tensorflow.keras import layers
+model_4_embedding = layers.Embedding(input_dim=max_vocab_length,
+                                     output_dim=128,
+                                     embeddings_initializer="uniform",
+                                     input_length=max_length,
+                                     name="embedding_4")
+
+# Build a Bidirectional RNN in TensorFlow
+inputs = layers.Input(shape=(1,), dtype="string")
+x = text_vectorizer(inputs)
+x = model_4_embedding(x)
+# x = layers.Bidirectional(layers.LSTM(64, return_sequences=True))(x) # stacking RNN layers requires return_sequences=True
+x = layers.Bidirectional(layers.LSTM(64))(x) # bidirectional goes both ways so has double the parameters of a regular LSTM layer
+outputs = layers.Dense(1, activation="sigmoid")(x)
+model_4 = tf.keras.Model(inputs, outputs, name="model_4_Bidirectional")
+
+# Compile
+model_4.compile(loss="binary_crossentropy",
+                optimizer=tf.keras.optimizers.Adam(),
+                metrics=["accuracy"])
+
+# Get a summary of our bidirectional model
+model_4.summary()
+
+# Fit the model (takes longer because of the bidirectional layers)
+model_4_history = model_4.fit(train_sentences,
+                              train_labels,
+                              epochs=5,
+                              validation_data=(val_sentences, val_labels),
+                              callbacks=[create_tensorboard_callback(SAVE_DIR, "bidirectional_RNN")])
+
+"""
+
+""" Conv1D model
+# Set random seed and create embedding layer (new embedding layer for each model)
+tf.random.set_seed(42)
+from tensorflow.keras import layers
+model_5_embedding = layers.Embedding(input_dim=max_vocab_length,
+                                     output_dim=128,
+                                     embeddings_initializer="uniform",
+                                     input_length=max_length,
+                                     name="embedding_5")
+
+# Create 1-dimensional convolutional layer to model sequences
+from tensorflow.keras import layers
+inputs = layers.Input(shape=(1,), dtype="string")
+x = text_vectorizer(inputs)
+x = model_5_embedding(x)
+x = layers.Conv1D(filters=32, kernel_size=5, activation="relu")(x)
+x = layers.GlobalMaxPool1D()(x)
+# x = layers.Dense(64, activation="relu")(x) # optional dense layer
+outputs = layers.Dense(1, activation="sigmoid")(x)
+model_5 = tf.keras.Model(inputs, outputs, name="model_5_Conv1D")
+
+# Compile Conv1D model
+model_5.compile(loss="binary_crossentropy",
+                optimizer=tf.keras.optimizers.Adam(),
+                metrics=["accuracy"])
+
+# Get a summary of our 1D convolution model
+model_5.summary()
+
+# Fit the model
+model_5_history = model_5.fit(train_sentences,
+                              train_labels,
+                              epochs=5,
+                              validation_data=(val_sentences, val_labels),
+                              callbacks=[create_tensorboard_callback(SAVE_DIR, 
+                                                                     "Conv1D")])
+
+OR
+
+# Create 1D convolutional model to process sequences
+inputs = layers.Input(shape=(1,), dtype=tf.string)
+text_vectors = text_vectorizer(inputs) # vectorize text inputs
+token_embeddings = token_embed(text_vectors) # create embedding
+x = layers.Conv1D(64, kernel_size=5, padding="same", activation="relu")(token_embeddings)
+x = layers.GlobalAveragePooling1D()(x) # condense the output of our feature vector
+outputs = layers.Dense(num_classes, activation="softmax")(x)
+model_1 = tf.keras.Model(inputs, outputs)
+
+# Compile
+model_1.compile(loss="categorical_crossentropy", # if your labels are integer form (not one hot) use sparse_categorical_crossentropy
+                optimizer=tf.keras.optimizers.Adam(),
+                metrics=["accuracy"])
+
+# Fit the model
+model_1_history = model_1.fit(train_dataset,
+                              steps_per_epoch=int(0.1 * len(train_dataset)), # only fit on 10% of batches for faster training time
+                              epochs=3,
+                              validation_data=valid_dataset,
+                              validation_steps=int(0.1 * len(valid_dataset))) # only validate on 10% of batches
+
+"""
+
+
+#4.5 mixed precision training
+# Turn on mixed precision training (that is to train with faster)
+# if you need check tensorflow.keras.mixed_precision
+""" mixed precison i.e train model with nvidia adv cuda
+from tensorflow.keras import mixed_precision
+mixed_precision.set_global_policy(policy="mixed_float16") # set global policy to mixed precision 
+
+mixed_precision.global_policy() # should output "mixed_float16" (if your GPU is compatible with mixed precision)
+
+example build model for mix precision you need to add dtype=tf.float32 to use mix precsion
+from tensorflow.keras import layers
+
+# Create base model
+input_shape = (224, 224, 3)
+base_model = tf.keras.applications.EfficientNetB0(include_top=False)
+base_model.trainable = False # freeze base model layers
+
+# Create Functional model 
+inputs = layers.Input(shape=input_shape, name="input_layer")
+# Note: EfficientNetBX models have rescaling built-in but if your model didn't you could have a layer like below
+# x = layers.Rescaling(1./255)(x)
+x = base_model(inputs, training=False) # set base_model to inference mode only
+x = layers.GlobalAveragePooling2D(name="pooling_layer")(x)
+x = layers.Dense(len(class_names))(x) # want one output neuron per class 
+# Separate activation of output layer so we can output float32 activations
+outputs = layers.Activation("softmax", dtype=tf.float32, name="softmax_float32")(x) 
+model = tf.keras.Model(inputs, outputs)
+
+# Compile the model
+model.compile(loss="sparse_categorical_crossentropy", # Use sparse_categorical_crossentropy when labels are *not* one-hot
+              optimizer=tf.keras.optimizers.Adam(),
+              metrics=["accuracy"])
+
+# Check the dtype_policy attributes of layers in our model
+for layer in model.layers:
+    print(layer.name, layer.trainable, layer.dtype, layer.dtype_policy) # Check the dtype policy of layers
+
+# Check the layers in the base model and see what dtype policy they're using
+for layer in model.layers[1].layers[:20]: # only check the first 20 layers to save output space
+    print(layer.name, layer.trainable, layer.dtype, layer.dtype_policy)
+
+"""
+

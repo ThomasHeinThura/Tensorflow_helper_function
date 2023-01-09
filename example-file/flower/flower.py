52099d86ddc4084256b124b805e7b5a2849184e0
ThomasHeinThura <alexzander125.alex@gmail.com>
2023-01-09 21:52:35 +0630

    add code

--- example-file/flower/flower.py
+++ example-file/flower/flower.py
@@ -1,9 +1,9 @@
 """
 The model performance : 
-val_accuary : 86%
-val_loss : 0.4491
-time : 53min
-epoch : 25 (get val_accuary 77.78% on 10 epoch and loss is 0.6628 time is 22min51sec)
+val_accuary : 59%
+val_loss : 1.0452
+time : 7min16sec
+epoch : 10 (get val_accuary 77.78% on 10 epoch and loss is 0.6628 time is 22min51sec)
 """
 
 import numpy as np
@@ -24,42 +24,10 @@ img_width = 128
 AUTOTUNE = tf.data.AUTOTUNE
 input_shape = (img_height,img_width, 3)
 num_classes = 5
-epoch = 10
-
-# data work thorugh dirpath for CNN
-def walk_through_dir(dir_path):
-    import os 
-    """
-    Walks through dir_path returning its contents.
-
-    Args:
-        dir_path (str): target directory
-  
-    Returns:
-        A print out of:
-        number of subdiretories in dir_path
-        number of images (files) in each subdirectory
-        name of each subdirectory
-    """
-    for dirpath, dirnames, filenames in os.walk(dir_path):
-        print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
-# Visualize classnes from dir
-def view_class_name_from_dir(path):
-    import pathlib 
-    import numpy as np
-    """
-    View an class name from import data folder
-    :param path:
-    :return print class_name:
-    """
-    data_dir = pathlib.Path(path)
-    class_name = np.array(sorted([item.name for item in data_dir.glob('*')]))
-    return print(class_name)
+epoch = 20
 
 # Import Data
 flower_dir = '/home/hanlinn/tensorflow_datasets/flowers/'
-flower_df = walk_through_dir(flower_dir)
-flower_class_name = view_class_name_from_dir(flower_dir)
 
 train_ds = tf.keras.utils.image_dataset_from_directory(
   flower_dir,
@@ -97,15 +65,18 @@ model = tf.keras.Sequential([
   tf.keras.layers.Input(shape=(input_shape),name='input_layers'),
   tf.keras.layers.Rescaling(1./255),
   tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
   tf.keras.layers.BatchNormalization(),
   tf.keras.layers.MaxPooling2D(),
   tf.keras.layers.Conv2D(64, 3, activation='relu'),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
   tf.keras.layers.BatchNormalization(),
   tf.keras.layers.MaxPooling2D(),
   tf.keras.layers.Conv2D(128, 3, activation='relu'),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
   tf.keras.layers.BatchNormalization(),
   tf.keras.layers.MaxPooling2D(),
-  tf.keras.layers.GlobalMaxPooling2D(),
+  tf.keras.layers.Flatten(),
   tf.keras.layers.Dropout(0.25),
   tf.keras.layers.Dense(128, activation='relu'),
   tf.keras.layers.Dense(64, activation='relu'),
